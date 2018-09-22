fa884c9f95453302752351616fd8c4f813e78192
CaptainCoca <sk8.web.admin@googlemail.com>
2018-09-22 22:44:26 +0200

    added validation message

--- src/app/services/auth-api.service.ts
+++ src/app/services/auth-api.service.ts
@@ -27,6 +27,7 @@ export class AuthApiService {
         });
         const data = await result.json();
         if (!data) { throw new Error('Response is not valid'); }
+        console.log('Validate token:', data);
         resolve(data.message.id > 0);
       } catch (err) {
         reject(err);
